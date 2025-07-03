db9410faa78bdb05f059237069a28792e2eb21eb
choo123 <cngnlcks@gmail.com>
2025-07-03 15:37:35 +0900

    half_solved_05

--- 05.Longest_Palindromic_Substring/LPS.go
+++ 05.Longest_Palindromic_Substring/LPS.go
@@ -1,11 +1,11 @@
+package main
+
 /*
 numbering each elements
 2>check
 find every possible combination
 vaildate
  */
-
-package main
 
 import (
 	"fmt"
@@ -16,20 +16,27 @@ func main() {
 	fmt.Println(longestPalindrome(input1))
 }
 
-func findin(c rune, i_list []rune) bool {
-	for _, v := range i_list {
-		if v == c {
-			return true
+func isitpalindrome(s_list []rune) bool {
+	for i, j := 0, len(s_list)-1; i < j; i, j = i+1, j-1 {
+		if s_list[i] != s_list[j] {
+			return false
 		}
 	}
-	return false
+	return true
 }
 
 func longestPalindrome(s string) string {
-	var i_list []rune
-	for i := 0; i<len(s);i++ {
-		i_list = append(i_list, s[i])
+	i_list := []rune(s)
+	answer := ""
+	for k := 0; k < len(i_list); k++ {
+		for l := k; l < len(i_list); l++ {
+			instance := i_list[k : l+1]
+			if isitpalindrome(instance) {
+				if len(instance) > len([]rune(answer)) {
+					answer = string(instance)
+				}
+			}
+		}
 	}
-
-
+	return answer
 }
